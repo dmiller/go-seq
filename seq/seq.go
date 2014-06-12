56ea7dce4a48285ded9a75618b9ff2abf43d3007
David Miller <dmiller2718@gmail.com>
2014-06-12 03:02:32 -0500

    Add seq/seq.go to provide package doc

---
+++ seq/seq.go
@@ -0,0 +1,6 @@
+// Copyright 2014 David Miller. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package seq provides implementations of a variety of immutable, persistent data structures.
+package seq
